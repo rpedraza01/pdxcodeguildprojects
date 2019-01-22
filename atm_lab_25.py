a454974713110f77dce02c8f72a73234b4403988
rpedraza01 <rpedraza01@gmail.com>
2019-01-21 17:17:45 -0800

    new commit 1/21/2019

--- atm_lab_25.py
+++ atm_lab_25.py
@@ -2,19 +2,25 @@
 
 class ATM:
     def __init__(self):
-        # self.balance begins as 
+        # self.balance begins as the int 0
         self.balance = 0
+        # self.transactions is an empty list
         self.transactions = []
+
 
     def check_balance(self):
         # returns the account balance
         return self.balance
 
+
     def deposit(self, amount_in):
         # deposits the given amount in the account
         self.balance += amount_in
+        # the variable message is created with the f-string
         message = f"You've deposited +${amount_in}"
+        # the list self.transactions is appended with the variable message
         self.transactions.append(message)
+        # returns the variable message
         return message
 
 
@@ -25,11 +31,16 @@ class ATM:
 
     def withdraw(self, amount_out):
         # withdraws the amount from the account and returns it
+        # creates the variable message with an f-string
         message = f"You've withdrawn -${amount_out}"
+        # the list self.transactions is appended with the variable message
         self.transactions.append(message)
+        # creates the if statement to see if there's enough money that can be withdrawn using the self.balance method to check if there's sufficient funds
         if self.check_withdrawl(amount_out):
+            # if there's sufficient funds, money is withdrawn and the variable message is printed
             self.balance -= amount_out
             print(message)
+        # if there's isn't enough funds to be withdrawn a statement stating such is printed
         else:
             print("Insufficient funds. Please withdraw an amount less than your account balance.")
 
@@ -48,25 +59,24 @@ class ATM:
         print("Transaction History.")
         valid_commands = ["deposit", "withdraw", "check balance", "transaction history", "exit"]
 
-atm = ATM()
-# print(atm.balance)
-# atm.check_balance()
-# print(atm.check_balance())
-# print(atm.deposit(10))
-# print(atm.balance)
-# atm.check_withdrawl(10)
-# print(atm.balance)
-# atm.withdraw(5)
-# print(atm.balance)
-# atm.print_transactions()
-# print(atm.print_transactions())
 
+# the class ATM is called by creating the instance of atm
+atm = ATM()
+
+
+# a while True loop is created where the program is run outside of the classes and methods
 while True:
+    # prints a welcome statement
     print("Welcome to ATM.")
+    # creates another while True loop
     while True:
+        # creates the list of command which has the recognized strings for the user to enter to use the prorgam
         command = ["deposit", "withdraw", "check balance", "transaction history", "exit"]
+        # creates the variable user_command which is modified by whatever command the user inputs
         user_command = input("What would you like to do today? (Deposit, Withdraw, Check Balance, Transaction History, Exit) > ")
+        # prints the users command
         print(user_command)
+        # an if loop is created that checks the user's command and executes it, checking that the command(s) are from the approved list in the variable command
         if user_command in command:
             if user_command == "deposit":
                 print(atm.deposit(int(input("How much would you like to deposit? > "))))
@@ -79,9 +89,7 @@ while True:
 
             elif user_command == "transaction history":
                 atm.print_transactions()
-
+            # if an invalid command is inputted the loop breaks
             else:
                 break
     break
-
-# tests
--- number_to_phrase_lab_15.py
+++ number_to_phrase_lab_15.py
@@ -1,35 +1,42 @@
 #number_to_phrase_lab_15.py
+# lines 3 - 5 are dictionaries with keys of an integer and values of strings of the numbers spelled out
 tens_dict = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"}
 ones_dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 teens_dict = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"}
-
+# line 7 creates the variable of number_input and asks the user for an integer input
 number_input = int(input("What number do you wish to wordify? > "))
+# line 9 creates the variable of number_word which is set to an empty list so it can later be modified
 number_word = []
+# line 11 is the beginning of the while loop
 while number_input > 0:
-	if number_input > 99:
-		hundred = number_input // 100
-		number_word.append(ones_dict[hundred] + " hundred")
-		number_input = number_input % 100
-	elif number_input >= 20:
-		tens = number_input // 10
-		number_word.append(tens_dict[tens])
-		number_input = number_input % 10
-	elif number_input > 9:
-		number_word.append(teens_dict[number_input])
-		break
-	elif number_input > 0:
-		number_word.append(ones_dict[number_input])
-		break
-	# 	if number_input > 99: 
-			
-	# 		print(hundred)
-	# 	tens = number_input // 10
-	# 	# print(tens_dict[tens])
-	# 	ones = number_input % 10
-	# 	print(tens_dict[tens] + "-" + ones_dict[ones])
-	# elif 19 >= number_input > 9:
-	# 	print(teens_dict[number_input])
-	# elif number_input <= 9:
-	# 	print(ones_dict[number_input])
+    # checks if the number_input is greater than 99, if so then it runs through the if statement
+    if number_input > 99:
+        # the variable hundred is created by using float division on number_input to remove the hundreds spot from a 3 digit number
+        hundred = number_input // 100
+        # the empty list number_word is appeneded with the ones_dict and the variable hundred and the string " hundred"
+        number_word.append(ones_dict[hundred] + " hundred")
+        # number_input is then modified by % 100 to get any remainder which will then take the number back to the beginning of the loop and proceed to the first elif
+        number_input = number_input % 100
+    # if number_input is greater than or equal to 20 it runs through the elif statement
+    elif number_input >= 20:
+        # the variable tens is created by using float division on number_input to remove the tens spot
+        tens = number_input // 10
+        # the list number_word is then appended with the tens_dict and the variable tens
+        number_word.append(tens_dict[tens])
+        # number_input is then modified by % 10 get any remainder which will then take the number back to the beginning of the loop and proceed to the next elif
+        number_input = number_input % 10
+    # checks if number_input is greater than 9
+    elif number_input > 9:
+        # the list number_word is then appended with teens_dict and number_input
+        number_word.append(teens_dict[number_input])
+        # the loop breaks if a number meets all of the if/elif statements and there's no remainder by this line
+        break
+    # checks to see if number_input is greater than 0
+    elif number_input > 0:
+        # the list number_word is appended with the ones_dict and number_input
+        number_word.append(ones_dict[number_input])
+        # if all if/elif statements are met then the loop breaks
+        break
 
+# prints out the now heavily modified list number_word and joins it with the string " " so that each word is separated by a space and is more readable by the user.
 print(" ".join(number_word))
