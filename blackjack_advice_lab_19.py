f8bf626ab3981d4a33d4edb311040bb97ea12dbf
rpedraza01 <rpedraza01@gmail.com>
2019-01-21 21:53:08 -0800

    new commit 2 1/21/2019

--- blackjack_advice_lab_19.py
+++ blackjack_advice_lab_19.py
@@ -1,1 +1,20 @@
 #blackjack_advice_lab_19.py
+
+print("Welcome to the Blackjack Advisor. Let's get you some wins!")
+num_card_dict = {1: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10"}
+face_card_dict = {10: "J" or "Q" or "K"} #{key: 10 for key in "JKQ"}
+card_hand = 0
+first_card = int(input("What is your first card? > "))
+card_hand += first_card
+second_card = int(input("What is your second card? > "))
+card_hand += second_card
+third_card = int(input("What is your third card? > "))
+card_hand += third_card
+if card_hand < 17:
+	print("I suggest you hit.")
+elif 21 > card_hand >= 17:
+	print("I advise you stay.")
+elif card_hand == 21:
+	print("You've got Blackjack! You win!")
+elif card_hand > 21:
+	print("You've busted. Better luck next time.")
---
+++ palindrome_anagram_lab_17.py
@@ -0,0 +1,36 @@
+#palindrome_anagram_lab_17.py
+
+def check_palindrome(palindrome):
+	palindrome = input("What is the word you wish to check is a palindrome? > ")
+	palindrome = palindrome.lower()
+	palindrome = palindrome.replace(" ", "")
+	palindrome_list = list(palindrome)
+	palindrome_list.sort()
+	palindrome_list.reverse()
+	if palindrome_list == True:
+		print("That's a palindrome.")
+	else:
+		print("That's not a palindrome.")
+
+
+def check_anagram(first_word, second_word):
+	first_word = input("What's your first word? > ")
+	second_word = input("What's your second word? > ")
+	first_word = first_word.lower()
+	second_word = second_word.lower()
+	first_word = first_word.replace(" ", "")
+	second_word = second_word.replace(" ", "")
+	first_list = list(first_word)
+	second_list = list(second_word)
+	first_list.sort()
+	second_list.sort()
+	if first_list == second_list:
+		print("That's an anagram.")
+	else:
+		print("That's not an anagram.")
+
+
+check_palindrome()
+
+
+check_anagram()
