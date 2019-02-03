a45d2190a93ea974389954a08ab839fd3669e573
rpedraza01 <rpedraza01@gmail.com>
2019-02-03 12:10:39 -0800

    html and css commit

--- python_mini_capstone.py
+++ python_mini_capstone.py
@@ -63,25 +63,3 @@ main()
 # print(response.headers)
 
 # print(response.headers["content-type"])
-
-
-
-
-
-
-
-
-
-
-# import markovify
-
-# with open("/path/to/my/corpus.txt") as f:
-# 	text = f.read()
-
-# text_model = markovify.Text(text)
-
-# for i in range(5):
-# 	print(text_model.make_sentence())
-
-# for i in range(3):
-# 	print(text_model.make_short_sentence(140))
